0c508e87ccdf0cd3ea561083d75637eeaa215e65
Agathamoraes <agatha.batista@pucpr.edu.br>
2021-05-26 22:48:51 -0300

    cadastro exercicio/treino

--- FitTech/src/fittech/Main.java
+++ FitTech/src/fittech/Main.java
@@ -1,30 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fittech;
 import Controller.MainController;
 
-<<<<<<< HEAD
 import Controller.Menu_Controller;
 
-/**
- *
- * @author Vinicius Taborda
- */
-=======
->>>>>>> master
 public class Main {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        // TODO code application logic here
         
         Menu_Controller menu = new Menu_Controller();
-=======
         MainController mc = new MainController();
->>>>>>> master
     }
 
 }
