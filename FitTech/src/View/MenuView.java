874ca19ec2bbc3d78d1cd099695c26bf0c717237
ViniciusTaborda <vinicius.taborda.costa@gmail.com>
2021-06-10 22:16:18 -0300

    ajustes

--- FitTech/src/View/MenuView.java
+++ FitTech/src/View/MenuView.java
@@ -10,6 +10,8 @@ public class MenuView {
     public int Menu() {
 
         Scanner sc = new Scanner(System.in);
+        System.out.println("\n BEM VINDO AO APPLICATIVO FITTECH");
+
         System.out.println("\n(1) Cadastrar Aluno");
         System.out.println("(2) Cadastrar Grupo Muscular");
         System.out.println("(3) Cadastrar Exercicio");
@@ -19,10 +21,15 @@ public class MenuView {
         System.out.println("(7) Login\n");
 
         System.out.println("(9) Sair\n");
-        System.out.print(" Opção: ");
+
+        System.out.println("Feito por:");
+        System.out.println("\t Agatha Moraes");
+        System.out.println("\t Hannah Coutinho");
+        System.out.println("\t Vinicius Taborda");
+
+        System.out.print("Opção: ");
 
         return sc.nextInt();
     }
-
 
 }
